853e01029d65b4f13c4f05c9c89a4f0cc5aaefd9
Gabriel Miranda <gabrielcm.br@gmail.com>
2018-05-28 13:33:47 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,17 +1,46 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data.each do |season_num, contestantarray|
+    if season_num == season
+      contestantarray.each do |contestant_info|
+        if contestant_info["status"] = "Winner"
+          winner = contestant_info["name"]
+          return winner.split(" ")[0]
+        end
+      end
+    end
+  end
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |season_num, contestantarray|
+    contestantarray.each do |contestant_info|
+      if contestant_info["occupation"] == occupation
+        return contestant_info["name"]
+      end
+    end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  counter = 0
+  data.each do |season_num, contestantarray|
+    contestantarray.each do |contestant_info|
+      if contestant_info["hometown"] == hometown
+        counter += 1
+      end
+    end
+  end
+  return counter
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |season_num, contestantarray|
+    contestantarray.each do |contestant_info|
+      if contestant_info["hometown"] == hometown
+        return contestant_info[]
+      end
+    end
+  end
 end
 
 def get_average_age_for_season(data, season)
